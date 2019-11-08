66a3b89e07967fce18809268de9f85b525b88de4
kitokiyohito <srrzhuaktm@icloud.com>
2019-11-08 20:12:29 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+ <div>
+  <h1>ようこそco-labへ</h1>
+  <NLink to="/about">
+   About Page
+  <NLink>
+   <br>
+  <NLink to="/photo">
+   Photo Library
+  </NLink>
+ </div>
+</template>]
+
+<script>
+
+export default {
+  components: {
+
+  }
+}
+</script>
